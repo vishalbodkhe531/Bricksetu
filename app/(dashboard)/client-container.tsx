cf947e3ac92450ff4b191a01386cabcb2fe1d4ce
Vishal <vishalbodkhe531@gmail.com>
2026-08-31 11:22:19 +0530

    feat: remove spacing from sidbar and main page

--- app/(dashboard)/client-container.tsx
+++ app/(dashboard)/client-container.tsx
@@ -24,13 +24,13 @@ export function ClientDashboardContainer({
         <MobileNav isOpen={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)} />
 
         {/* Main Content Area */}
-        <div className="flex-1 lg:pl-64 flex flex-col min-w-0">
+        <div className="flex-1 lg:pl-63 flex flex-col min-w-0">
           <Header
             user={user}
             onMobileMenuToggle={() => setMobileMenuOpen((prev) => !prev)}
           />
 
-          <main className="flex-1 p-4 sm:p-6 lg:p-8 max-w-7xl w-full mx-auto">
+          <main className="flex-1 p-4 sm:p-6 lg:p-8 max-w-9xl w-full mx-auto">
             {children}
           </main>
         </div>
